e8c735f449df5bdad60e496243b9dd411d6fb9b2
Josh Attenberg <jattenberg@etsy.com>
2012-05-30 09:58:59 -0400

    adding cdf to histogram

--- scripts/thist.pl
+++ scripts/thist.pl
@@ -12,8 +12,8 @@ $rtot = 0;
 for($i = 0; $i < @all; $i++)
 {
 	@p = @{$all[$i]};
-	#$rtot += $p[-1]/$tot;
+	$rtot += $p[-1]/$tot;
 	push(@p,$p[-1]/$tot);
-	#push(@p,$rtot);
+	push(@p,$rtot);
 	print join("\t",@p),"\n";
 }
