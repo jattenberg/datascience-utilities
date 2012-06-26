fda691d789fdfabc457d618d35be43edac1c6fb6
Josh Attenberg <jattenberg@etsy.com>
2012-06-26 12:53:44 -0400

    updating plotXY

--- scripts/plotXY.py
+++ scripts/plotXY.py
@@ -13,7 +13,8 @@ from optparse import OptionParser
 #expects a list of pairs, x, #x
 
 parser = OptionParser(usage ="""                                                                 
-Generate a plot from new-line separated numerical data of the form x,y
+Generate a plot from new-line separated numerical data of the form x,[y]
+where [y] is a list of y values, each associated with the same x.
 Usage %prog [options]                                                                                
 """)
 parser.add_option('-f', '--file',
@@ -26,6 +27,10 @@ parser.add_option('-y', '--ylabel',
                   action = 'store', dest = 'ylabel', default=False)
 parser.add_option('-L', '--logscale',
                   action = 'store_true', dest = 'log_scale', default=False)
+parser.add_option('-X', '--semilogx',
+                  action = 'store_true', dest = 'semilogx', default=False)
+parser.add_option('-Y', '--semilogy',
+                  action = 'store_true', dest = 'semilogy', default=False)
 
 (options, args) = parser.parse_args()
  
@@ -33,10 +38,18 @@ input = open(options.filename, 'r') if options.filename else sys.stdin
 
 xs = [] 
 ys = []
+cols = -1
+
 for line in input:
-    x, y = line.rstrip().split()
-    xs.append(log(float(x)) if options.log_scale else float(x))
-    ys.append(log(float(y)) if options.log_scale else float(y))
+    vals = line.rstrip().split()
+    x = vals.pop(0);
+    xs.append(log(float(x)) if options.log_scale or options.semilogx else float(x))
+    y = [log(float(z)) if options.log_scale or options.semilogy else float(z) for z in vals]
+    if cols == -1:
+        cols = len(y)
+    elif cols != len(y):
+        raise Exception("inconsistent number of x values. have observed %d and %d" % cols, len(y)) 
+    ys.append(y)
 
 marker = options.marker if options.marker else 'r.'
 
