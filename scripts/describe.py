00cfd74830e95444db03d74c89a0f50072449b02
Josh Attenberg <jattenberg@etsy.com>
2012-12-23 10:37:33 -0500

    bayesian confidence intervals arround mean, variance, and std dev

--- scripts/describe.py
+++ scripts/describe.py
@@ -1,12 +1,15 @@
 #!/usr/local/bin/python
-from scipy.stats import mstats, normaltest, sem
+from scipy.stats import mstats, normaltest, sem, bayes_mvs
 import sys
 from optparse import OptionParser
 import numpy as np
 import pandas as pd
 
 
-def get_data(column, np_values):
+def get_data(column, np_values, alpha):
+
+    mvs = bayes_mvs(np_values, alpha)
+
     output = [
         present("Column", column),
         present("Length", len(np_values)),
@@ -15,24 +18,27 @@ def get_data(column, np_values):
         present("Max", np_values.max()),
         present("Mid-Range", (np_values.max() - np_values.min())/2),
         present("Range", np_values.max() - np_values.min()),
+        present("Mean", np_values.mean(), mvs[0][1]),
+        present("Variance", mvs[1][0], mvs[1][1]),
+        present("StdDev", mvs[2][0], mvs[2][1]),
+        present("Mode", mstats.mode(np_values)[0][0]),
         present("Q1", mstats.scoreatpercentile(np_values, 25)),
         present("Q2", mstats.scoreatpercentile(np_values, 50)),
         present("Q3", mstats.scoreatpercentile(np_values, 75)),
         present("Trimean", trimean(np_values)),
         present("Minhinge", midhinge(np_values)),
-        present("Mean", np_values.mean()),
-        present("Variance", mstats.variation(np_values)),
-        present("StdDev", np.std(np_values)),
-        present("Mode", mstats.mode(np_values)[0][0]),
         present("Skewness", mstats.skew(np_values)),
         present("Kurtosis", mstats.kurtosis(np_values)),
         present("StdErr", sem(np_values)), 
-        present("Normal-P-value", normaltest(np_values)[1])
+        present("Normal-P-value", normaltest(np_values)[1]),
         ]
     return output
 
-def present(key, value):
-    return key + " : " + str(value)
+def present(key, value, bounds = None):
+    if bounds:
+        return key + " : " + str(value) + " : (%s, %s)" % (bounds[0], bounds[1]) 
+    else: 
+        return key + " : " + str(value)
 
 def trimean(values):
     return (mstats.scoreatpercentile(values, 25) + 2.0*mstats.scoreatpercentile(values, 50) + mstats.scoreatpercentile(values, 75))/4.0
@@ -52,6 +58,8 @@ parser.add_option('-H', '--header',
                   action = 'store_true', dest = 'header', default = None)
 parser.add_option('-d', '--delim',
                   action='store', dest='delim', default="\t") 
+parser.add_option('-a', '--alpha',
+                  action='store', dest='alpha', default=0.9) 
 
 (options, args) = parser.parse_args()
 
@@ -62,7 +70,7 @@ df = pd.read_csv(input, sep = options.delim,
 
 description = []
 for column in df.columns:
-    output = get_data(column, df[column].values)
+    output = get_data(column, df[column].values, float(options.alpha))
     description.append("\n".join(output))
 
 print "\n\n".join(description)
