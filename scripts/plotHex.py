3361d0a8ee1ef1c47112f90c0415a1c1a4aea3b0
Josh Attenberg <jattenberg@etsy.com>
2012-12-24 21:46:07 -0500

    optimizing plotHex

--- scripts/plotHex.py
+++ scripts/plotHex.py
@@ -18,15 +18,26 @@ each column containing values that are to be compared in the hexplot
 Usage %prog [options]                                                                                
 """)
 parser.add_option('-f', '--file',
-                  action = 'store', dest = 'filename', default=False)
+                  action = 'store', dest = 'filename', default=False,
+                  help="[optional] use a specified file instead of reading from stdin")
 parser.add_option('-b', '--bins', 
-                  action='store', dest='bins', default=20)
+                  action='store', dest='bins', default=20,
+                  help="number of bins in histogram (default 20)")
 parser.add_option('-d', '--delim', 
                   action='store', dest='delim', default="\t")
 parser.add_option('-H', '--header', 
-                  action='store_true', dest='header', default=None)
+                  action='store_true', dest='header', default=None,
+                  help="treat the first row as column headers")
 parser.add_option('-L', '--logscale',                                                                                                                                                                                                                                                                                      
-                  action = 'store_true', dest = 'log_scale', default=False)
+                  action = 'store_true', dest = 'log_scale', default=False,
+                  help="store log counts in each bin in the histogram")
+parser.add_option('-x', '--xval',
+                 action = 'store', dest = 'x', default = "0",
+                 help = "column used for the x axis")
+parser.add_option('-y', '--yval',
+                 action = 'store', dest = 'y', default = "1",
+                 help = "column used for the y axis")
+
 
 (options, args) = parser.parse_args()
  
@@ -37,11 +48,27 @@ df = pd.read_csv(input, sep = options.delim,
                  header = 0 if options.header else None)
 
 
-hexbin(df[df.columns[0]].values, df[df.columns[1]].values,
+
+if options.x in df.columns:
+    x = df[options.x]
+elif options.x.isdigit() and int(options.x) < df.shape[1]:
+    x = df.icol(int(options.x))
+else:
+    raise LookupError("Unknown X column: %s" % options.x)
+
+if options.y in df.columns:
+    y = df[options.y]
+elif options.x.isdigit()  and int(options.y) < df.shape[1]:
+    y = df.icol(int(options.y))
+else:
+    raise LookupError("Unknown X column: %s" % options.x)
+
+
+hexbin(x.values, y.values,
        gridsize=int(options.bins),
        bins="log" if options.log_scale else None) 
-xlabel(df.columns[0])
-ylabel(df.columns[1])
+xlabel(x.name)
+ylabel(y.name)
 colorbar()
 show()
 
