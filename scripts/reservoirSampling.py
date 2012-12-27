6a28cc4a5c018adf7313e77fb6c8f0aa755b3876
Josh Attenberg <jattenberg@etsy.com>
2012-12-27 09:08:32 -0500

    clean up in reservoir sampling

--- scripts/reservoirSampling.py
+++ scripts/reservoirSampling.py
@@ -25,8 +25,8 @@ sample = [];
 for i, line in enumerate(input):
     if i < N:
         sample.append(line)
-    elif i >= N and random.random() < N/float(i+1):
-        replace = random.randint(0,len(sample)-1)
+    elif i >= N and random.random() < N/float(i + 1):
+        replace = random.randint(0, len(sample) - 1)
         sample[replace] = line
  
 for line in sample:
