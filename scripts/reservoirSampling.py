31695867949961c398615b630ca80bf08ff3e952
Josh Attenberg <jattenberg@etsy.com>
2012-12-27 09:07:52 -0500

    more n-setting options for reservoir sampling

--- scripts/reservoirSampling.py
+++ scripts/reservoirSampling.py
@@ -12,16 +12,17 @@ parser.add_option('-f', '--file',
                   action = 'store', dest = 'filename', default=False,
                   help="[optional] use a specified file instead of reading from stdin")
 parser.add_option('-n', '--number',
-                  action='store', dest='n', default=100,                                                      
+                  action='store', dest='n', default=False,
                   help="the number of input lines to keep") 
 
 (options, args) = parser.parse_args()
-N = int(options.n)
+
+N = int(options.n) if options.n else int(args[0]) if len(args) >= 1 else 100
 input = open(options.filename, 'r') if options.filename else sys.stdin 
 
 sample = [];
- 
-for i,line in enumerate(input):
+
+for i, line in enumerate(input):
     if i < N:
         sample.append(line)
     elif i >= N and random.random() < N/float(i+1):
