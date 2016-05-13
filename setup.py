6bb8d4eedc738d721283487012873275bb925962
Josh Attenberg <josh.attenberg@gmail.com>
2016-05-13 10:39:29 -0400

    making this pythonic i think

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='datascience-utils',
+      version='1.0',
+      description="Various data analysis scripts, useful for command line data mining and exploration. Most scripts work on csv numeric data with some configurable separator, defaulting to \t, with optional header-handling.",
+      author="josh attenberg",
+      author_email="jattenbe@stern.nyu.edu",
+      url="https://github.com/jattenberg/datascience-utilities",
+      packages=['datascience-utils']
+)
