cb38e5ec8441275e6a6c60dcfcfa653636b1321f
Josh Attenberg <josh.attenberg@gmail.com>
2019-02-22 12:32:11 -0500

    main for ztest

--- datascience_utilities/ztest.py
+++ datascience_utilities/ztest.py
@@ -23,17 +23,22 @@ THE SOFTWARE.
 import sys
 from math import sqrt
 
-treatment_successes = float(sys.argv[1])
-treatment_failures = float(sys.argv[2])
+def main():
 
-baseline_successes = float(sys.argv[3])
-baseline_failures = float(sys.argv[4])
-n_treatment = treatment_successes + treatment_failures
-n_baseline = baseline_successes + baseline_failures
+    treatment_successes = float(sys.argv[1])
+    treatment_failures = float(sys.argv[2])
 
-p_treatment = treatment_successes/n_treatment
-p_baseline = baseline_successes/n_baseline
+    baseline_successes = float(sys.argv[3])
+    baseline_failures = float(sys.argv[4])
+    n_treatment = treatment_successes + treatment_failures
+    n_baseline = baseline_successes + baseline_failures
 
-p_hat = (treatment_successes + baseline_successes)/(n_treatment + n_baseline)
+    p_treatment = treatment_successes/n_treatment
+    p_baseline = baseline_successes/n_baseline
 
-print (abs((p_treatment - p_baseline)/sqrt(p_hat*(1-p_hat)*(1.0/n_baseline + 1.0/n_treatment))))
+    p_hat = (treatment_successes + baseline_successes)/(n_treatment + n_baseline)
+
+    print (abs((p_treatment - p_baseline)/sqrt(p_hat*(1-p_hat)*(1.0/n_baseline + 1.0/n_treatment))))
+
+if __name__ == "__main__":
+    main()
