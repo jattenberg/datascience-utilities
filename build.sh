725b859563185456d1ea5cd5b314551ab530c105
Josh Attenberg <josh.attenberg@gmail.com>
2020-10-03 17:27:37 -0400

    updating build script

--- build.sh
+++ build.sh
@@ -3,20 +3,10 @@
 # assumes python 3.6, pip, virtualenv
 iterm=$1
 
-
-
-hash virtualenv
-if [ "$?" != "0" ]; then
-    pip install virtualenv;
-fi
-
 hash pipx
 if [ "$?" != "0" ]; then
   pip install --user pipx;
 fi
-
-virtualenv venv
-source venv/bin/activate
 
 if [ "$iterm" = "iterm" ]; then
     echo "building with iterm inline plotting"
